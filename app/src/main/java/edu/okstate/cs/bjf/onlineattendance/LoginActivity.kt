23f513cac160eab1cecb77d0b8f5abfed212c4b2
Brandon Fletcher <brandonjfletcher@gmail.com>
2020-04-19 18:05:17 -0500

    Login functionality now works for teacher, will need to create a condition to check if it is a student/teacher that is signing in.

--- app/src/main/java/edu/okstate/cs/bjf/onlineattendance/LoginActivity.kt
+++ app/src/main/java/edu/okstate/cs/bjf/onlineattendance/LoginActivity.kt
@@ -1,12 +1,58 @@
 package edu.okstate.cs.bjf.onlineattendance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_create_teacher_profile.*
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.loginButton
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener {
+            loginAccount(emailFieldEditText.text.toString(), passwordFieldEditText.text.toString());
+
+        }
+    }
+
+    // Create a new user with a first and last name
+    private fun loginAccount(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(LoginActivity.TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    if (user != null) {
+                        // TODO: Check teacher/student & do intent accordingly. Currently goes teacher only.
+                        val teacherIntent = Intent(this, TeacherProfile::class.java)
+                        startActivity(teacherIntent)
+                    }
+                    // updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(LoginActivity.TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                    // updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    companion object {
+        const val TAG = "EmailPassword"
     }
 }
--- app/src/main/java/edu/okstate/cs/bjf/onlineattendance/TeacherProfile.kt
+++ app/src/main/java/edu/okstate/cs/bjf/onlineattendance/TeacherProfile.kt
@@ -1,12 +1,61 @@
 package edu.okstate.cs.bjf.onlineattendance
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class TeacherProfile : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    var user = FirebaseAuth.getInstance().currentUser
+    var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teacher_profile)
+
+        auth = FirebaseAuth.getInstance()
+        updateUI()
     }
+
+    private fun updateUI() {
+        if (user != null) {
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            val uid = user!!.uid
+            getTeacherName()
+        }
+    }
+
+    private fun getTeacherName() {
+        val uid = user!!.uid
+
+        db.collection("teachers")
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+
+                        if (document["teacher"] == uid) {
+                            println("Teacher Name: " + document["firstName"] + " " + document["lastName"])
+                        } else {
+                            Log.d(
+                                "Document",
+                                document.id + " => " + document.data
+                            )
+                        }
+
+                    }
+                } else {
+                    Log.w("Empty", "Error getting documents.", task.exception)
+                }
+            }
+    }
+
 }

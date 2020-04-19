4adf4fcceda5372f5cf8b138e8080ddce4f861a0
Brandon Fletcher <brandonjfletcher@gmail.com>
2020-04-19 17:19:26 -0500

    Successfully made the push of the profile picture to Firebase Storage.
    Once successfully creating an account, it takes the user to the
    LoginActivity.

--- app/src/main/java/edu/okstate/cs/bjf/onlineattendance/CreateTeacherProfileActivity.kt
+++ app/src/main/java/edu/okstate/cs/bjf/onlineattendance/CreateTeacherProfileActivity.kt
@@ -4,25 +4,34 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_create_teacher_profile.*
+import java.io.ByteArrayOutputStream
 
 
 class CreateTeacherProfileActivity : AppCompatActivity() {
 
     /* Resource Found On How To Choose Profile Picture: https://devofandroid.blogspot.com/2018/09/pick-image-from-gallery-android-studio_15.html  */
 
-    // Variables Needed via Firebase
-    private var mAuth: FirebaseAuth? = null
-    var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    // Firebase variables
+    private lateinit var auth: FirebaseAuth
+    var db = FirebaseFirestore.getInstance()
+    private var storageRef: StorageReference? = null
+
+    private lateinit var imageUri : Uri
+
+    private lateinit var profileBitmap: Bitmap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,11 +52,14 @@ class CreateTeacherProfileActivity : AppCompatActivity() {
             }
         }
 
-        mAuth = FirebaseAuth.getInstance();
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        // mStorageRef = FirebaseStorage.getInstance().reference;
+        // [END initialize_auth]
 
         submitNewTeacherProfile.setOnClickListener {
-            // createAccount(emailEditText.text.toString(), passwordEditText.text.toString());
-            // addCourseToFirestore()
+            createAccount(emailEditText.text.toString(), passwordEditText.text.toString());
 
         }
     }
@@ -59,12 +71,6 @@ class CreateTeacherProfileActivity : AppCompatActivity() {
         startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
-    companion object {
-        //image pick code
-        private const val IMAGE_PICK_CODE = 1000
-        //Permission code
-        const val PERMISSION_CODE = 1001
-    }
 
     //handle requested permission result
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -88,80 +94,110 @@ class CreateTeacherProfileActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
             chooseProfilePcitureButton.setImageURI(data?.data)
+            profileBitmap = (chooseProfilePcitureButton.drawable as BitmapDrawable).bitmap
         }
     }
 
-    /**
-     * Commenting out the below code, so we may try to re-implement adding a teacher
-     * to the Firebase database. This works, but isn't the type of functionality
-     * we are looking for.
-     *
+    // Create a new user with a first and last name
     private fun createAccount(email: String, password: String) {
-        Log.d("Created Account", "createAccount:$email")
-        mAuth!!.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(
-                this
-            ) { task ->
+        Log.d(TAG, "createAccount:$email")
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    Log.d("Created User", "createUserWithEmail:success")
-                    val user = mAuth!!.currentUser
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    if (user != null) {
+                        createCourse(user.uid.toString(), courseNameEditText.text.toString())
+                        uploadImageAndSaveUri(profileBitmap)
+                        val loginIntent = Intent(this, LoginActivity::class.java)
+                        createTeacher(user.uid.toString(), firstNameEditText.text.toString(), lastNameEditText.text.toString(), emailEditText.text.toString())
+                        startActivity(loginIntent)
+                    }
                     // updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
-                    Log.w(
-                        "Failed User Creation",
-                        "createUserWithEmail:failure",
-                        task.exception
-                    )
-                }
-
-                // ...
-            }
-    }
-
-    private fun addUserNameToUser(user: FirebaseUser?) {
-        val username: String = firstNameEditText.text.toString() + " " + lastNameEditText.text.toString()
-        val profileUpdates = UserProfileChangeRequest.Builder()
-            .setDisplayName(username)
-            .build()
-        user!!.updateProfile(profileUpdates)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Log.d("Username: ", "User profile updated.")
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    // updateUI(null)
                 }
             }
+        // [END create_user_with_email]
     }
 
-    private fun addCourseToFirestore() {
-        // Create a new user with a first and last name
-
-        // Create a new user with a first and last name
-        val course: MutableMap<String, Any> = HashMap()
-        course["teacher"] = firstNameEditText.text.toString() + " " + lastNameEditText.text.toString()
-        course["teacher-email"] = emailEditText.text.toString()
-        course["course-name"] = courseNameEditText.text.toString()
-        course["course-start"] = "0000"
-        course["course-end"] = "0000"
-        course["id"] = "0"
-        course["building"] = "Bldg 1"
-        course["room"] = "Rm 101"
-
-
+    private fun createCourse(teacher: String, courseName: String) {
+        val course = hashMapOf(
+            "teacher" to teacher,
+            "course" to courseName
+        )
 
         // Add a new document with a generated ID
-        db.collection("Courses")
+        db.collection("courses")
             .add(course)
             .addOnSuccessListener { documentReference ->
-                Log.d(
-                    "Course Added",
-                    "DocumentSnapshot added with ID: " + documentReference.id
-                )
-                println("Made it!")
-                val teacherProfileIntent = Intent(this, TeacherProfile::class.java)
-                startActivity(teacherProfileIntent)
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
             }
-            .addOnFailureListener { e -> Log.w("Course Not Added", "Error adding document", e) }
-    } */
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
+
+    private fun createTeacher(teacher: String, firstName: String, lastName: String, email: String) {
+        val course = hashMapOf(
+            "teacher" to teacher,
+            "firstName" to firstName,
+            "lastName" to lastName,
+            "email" to email
+        )
+
+        db.collection("teachers")
+            .add(course)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
+
+    private fun uploadImageAndSaveUri(bitmap: Bitmap) {
+        val baos = ByteArrayOutputStream()
+        val storageRef = FirebaseStorage.getInstance()
+            .reference
+            .child("pics/${FirebaseAuth.getInstance().currentUser?.uid}")
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val image = baos.toByteArray()
+
+        val upload = storageRef.putBytes(image)
+
+        upload.addOnCompleteListener { uploadTask ->
+            // progressbar_pic.visibility = View.INVISIBLE
+
+            if (uploadTask.isSuccessful) {
+                storageRef.downloadUrl.addOnCompleteListener { urlTask ->
+                    urlTask.result?.let {
+                        imageUri = it
+                        // activity?.toast(imageUri.toString())
+                        // image_view.setImageBitmap(bitmap)
+                    }
+                }
+            } else {
+                uploadTask.exception?.let {
+                    // activity?.toast(it.message!!)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+        // private const val RC_MULTI_FACTOR = 9005
+        //image pick code
+        private const val IMAGE_PICK_CODE = 1000
+        //Permission code
+        const val PERMISSION_CODE = 1001
+    }
 
 }

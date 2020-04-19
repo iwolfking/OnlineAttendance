b37591d9fb5bea60cd803c3824594054464971d8
Brandon Fletcher <brandonjfletcher@gmail.com>
2020-04-19 11:19:11 -0500

    Adding Student/Teacher activites, to be able to transition to after being signed in.

---
+++ app/src/main/java/edu/okstate/cs/bjf/onlineattendance/StudentProfile.kt
@@ -0,0 +1,12 @@
+package edu.okstate.cs.bjf.onlineattendance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_profile)
+    }
+}
---
+++ app/src/main/java/edu/okstate/cs/bjf/onlineattendance/TeacherProfile.kt
@@ -0,0 +1,12 @@
+package edu.okstate.cs.bjf.onlineattendance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeacherProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teacher_profile)
+    }
+}
